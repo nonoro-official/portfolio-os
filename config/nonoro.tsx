092ef8261331bb3dc6eb0b966bf201ede6545df4
Noah Peñaranda <noah.c.penaranda@gmail.com>
2026-07-01 15:04:59 +0800

    feat: updated and added data for desktop apps & files
    - added data for skills & about me
    - reorganized app grid locations
    - renamed settings to task manager
    - renamed readme to noro_ver

---
+++ config/nonoro.tsx
@@ -0,0 +1,52 @@
+import {
+  Code,
+  MonitorCog,
+  Database,
+  MonitorSmartphone,
+  PencilRuler,
+} from "lucide-react";
+
+export const about = [
+  {
+    icon: "🍋",
+    name: "About Me",
+    stack: "MongoDB - Express - React - Node.js",
+    desc: "Green Light Records is a records shop.",
+    preview: "💬",
+  },
+];
+
+export const skills = [
+  {
+    icon: <Code />,
+    name: "Languages",
+    stack:
+      " Java, Kotlin, C++, C#, Bash, Python, JavaScript, TypeScript, Dart, SQL, HTML, CSS",
+    preview: "💬",
+  },
+  {
+    icon: <MonitorCog />,
+    name: "Frameworks",
+    stack: "React, Next.js, Node.js, Express, Flutter, Flask",
+    preview: "🛍️",
+  },
+  {
+    icon: <Database />,
+    name: "Firebase, MongoDB, MySQL, Google Cloud",
+    stack: "WordPress",
+    preview: "💬",
+  },
+  {
+    icon: <MonitorSmartphone />,
+    name: "Systems & Platforms",
+    stack: "Linux, Raspberry Pi",
+    preview: "💬",
+  },
+  {
+    icon: <PencilRuler />,
+    name: "Design & Tools",
+    stack:
+      "WordPress, FlutterFlow, Wix, Canva, Figma, Penpot, draw.io, Git, ngrok",
+    preview: "📊",
+  },
+];
