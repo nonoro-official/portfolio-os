27f9c0ac5f0a5b7519b6de81d62484d22d8c3c1e
nonoro-official <noah.c.penaranda@gmail.com>
2026-09-03 18:48:54 +0800

    feat: made opening windows as single tap for mobile

--- components/desktop/draggables/DraggableDesktopItem.tsx
+++ components/desktop/draggables/DraggableDesktopItem.tsx
@@ -10,21 +10,26 @@ interface DraggableDesktopItemProps {
 export const DraggableDesktopItem: React.FC<DraggableDesktopItemProps> = ({
   item,
 }) => {
-  const { openWindow } = useDesktopContext();
+  const { openWindow, isMobile } = useDesktopContext();
   const { ref } = useDraggable({ id: item.id });
+
   const handleOpenItem = () => openWindow(item);
+
+  const eventHandlers = isMobile
+    ? { onClick: handleOpenItem }
+    : { onDoubleClick: handleOpenItem };
 
   return (
     <div
       ref={ref}
-      onDoubleClick={handleOpenItem}
+      {...eventHandlers}
       className="flex flex-col items-center justify-center w-full max-w-full text-center cursor-grab select-none active:cursor-grabbing p-1"
     >
       {/* Icon Display */}
       <div className="text-[36px] mb-1 drop-shadow-md">{item.icon}</div>
 
       {/* Label Text */}
-      <span className="text-foreground text-xs text-shadow-sm font-medium wrap-break-word line-clamp-2 px-1">
+      <span className="text-foreground text-xs text-shadow-sm font-medium wrap-break-words line-clamp-2 px-1">
         {item.name}
       </span>
     </div>
