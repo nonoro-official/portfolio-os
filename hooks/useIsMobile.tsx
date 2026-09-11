d6a61a9e18f00a58cd4bb9a3680bfb9ad9215daf
nonoro-official <noah.c.penaranda@gmail.com>
2026-09-11 20:03:34 +0800

    feat: mobile device check for mobile bootsplash & login
    - checks if portrait/phone, spoofed ipad, or android tablet
    - landscape devices will still have desktop ui but have mobile
    bootsplash & login

--- hooks/useIsMobile.tsx
+++ hooks/useIsMobile.tsx
@@ -1,12 +1,28 @@
 "use client";
-
 import { useState, useEffect } from "react";
 
-export function useIsMobile(breakpoint = 768) {
+export function useIsMobile(breakpoint = 1024) {
   const [isMobile, setIsMobile] = useState<boolean>(false);
 
   useEffect(() => {
-    const checkMobile = () => setIsMobile(window.innerWidth < breakpoint);
+    const checkMobile = () => {
+      // portrait
+      const isNarrowScreen = window.innerWidth < breakpoint;
+
+      //
+      const isSpoofedIPad =
+        navigator.userAgent.includes("Mac") &&
+        "maxTouchPoints" in navigator &&
+        navigator.maxTouchPoints > 2;
+
+      const isMobileAgent =
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          navigator.userAgent,
+        );
+
+      setIsMobile(isNarrowScreen || isSpoofedIPad || isMobileAgent);
+    };
+
     checkMobile(); // Check on initial mount
 
     window.addEventListener("resize", checkMobile);
